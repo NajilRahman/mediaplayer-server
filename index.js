77e29b6873745ba513e028758f40d97f80180181
Najil Rahman PM <sunaidnk1.gmail.com>
2024-08-26 22:39:25 +0530

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+const jsonServer=require('json-server');
+const middleware = jsonServer.defaults();
+const router = jsonServer.router('data.json')
+
+const app= jsonServer.create();
+
+app.use(middleware)
+app.use(router)
+
+const PORT=3000 || Process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log('running at port :'+PORT)
+})
+
+app.get('/video', (req, res) => {
+    res.json({ message: 'This is a custom route!' });
+});
+
+app.get('/history', (req, res) => {
+    res.json(console.log('history updated'));
+});
+
+app.get('/user', (req, res) => {
+    res.json(console.log('user'));
+});
